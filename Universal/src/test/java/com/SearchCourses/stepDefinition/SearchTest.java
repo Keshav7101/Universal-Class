5fb0490f1a720e83504ecb6fe477e6d73fc97dd5
kechoudh <kechoudh@INLEN8520065546.corp.capgemini.com>
2022-04-26 19:55:02 +0530

    Search step definition

--- Universal/src/test/java/com/SearchCourses/stepDefinition/SearchTest.java
+++ Universal/src/test/java/com/SearchCourses/stepDefinition/SearchTest.java
@@ -1,5 +1,85 @@
 package com.SearchCourses.stepDefinition;
 
-public class SearchTest {
+import static org.testng.Assert.assertEquals; 
+import static org.testng.Assert.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+
+public class SearchTest {
+	@Given("Website is ready")
+	public void website_is_ready() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I launch the url in chrome browser")
+	public void i_launch_the_url_in_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("A search box should be visible")
+	public void a_search_box_should_be_visible() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The search box should be in the body of the webpage")
+	public void the_search_box_should_be_in_the_body_of_the_webpage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I launch url in chrome bowser")
+	public void i_launch_url_in_chrome_bowser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should see the search button")
+	public void the_user_should_see_the_search_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The search button should be clickable")
+	public void the_search_button_should_be_clickable() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I launch URL in chrome browser")
+	public void i_launch_url_in_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Type the course name in search")
+	public void type_the_course_name_in_search() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User gets result according to the text entered")
+	public void user_gets_result_according_to_the_text_entered() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
